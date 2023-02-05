f362ad60e8591fde7130528f148735cef172bf5d
Sanskar Sisodia <500091107@stu.upes.ac.in>
2023-02-05 16:58:15 +0530

    first commit

---
+++ src/main/java/mypkg/MyClass.java
@@ -0,0 +1,15 @@
+package mypkg;
+
+public class MyClass {
+	public void show()
+	{
+		System.out.print("Hello Devops! Welcome to Jenlins");
+	}
+
+	public static void main(String[] args) {
+		MyClass ob=new MyClass();
+		ob.show();
+		
+	}
+
+}
