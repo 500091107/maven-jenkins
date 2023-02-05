ec5e7f5d970534d86a98ff28e2e8eb9cfbddc5f9
Sanskar Sisodia <500091107@stu.upes.ac.in>
2023-02-05 19:34:55 +0530

    Fifth Commit

--- src/main/java/mypkg/MyClass.java
+++ src/main/java/mypkg/MyClass.java
@@ -11,11 +11,17 @@ public class MyClass {
 		System.out.println("Hello Devops! Welcome to Jenlins");
 		
 	}
+	public void showsname()
+	{
+		System.out.println("Hello Sanskar");
+		
+	}
 
 	public static void main(String[] args) {
 		MyClass ob=new MyClass();
 		ob.show();
 		ob.shows();
+		ob.showsname();
 	}
 
 }
