cc7172cd9f7e6e45a70c767db45dd377c3796de1
Sanskar Sisodia <500091107@stu.upes.ac.in>
2023-02-05 19:15:17 +0530

    abcd

--- src/main/java/mypkg/MyClass.java
+++ src/main/java/mypkg/MyClass.java
@@ -1,15 +1,20 @@
 package mypkg;
 
 public class MyClass {
-	public void show()
-	{
+	public void show() {
 		System.out.print("Hello Devops! Welcome to Jenlins");
+
+	}
+
+	public void display() {
+		System.out.print("CICD Pipeline");
+
 	}
 
 	public static void main(String[] args) {
-		MyClass ob=new MyClass();
+		MyClass ob = new MyClass();
 		ob.show();
-		
+
 	}
 
 }
